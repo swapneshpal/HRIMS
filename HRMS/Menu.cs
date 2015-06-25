041e7946b4a62ec5af1294d1cd4a3b1d75524ae7
swapneshpal <swapnesh.pal@a4technology.com>
2015-06-25 18:13:35 +0530

    Today changes

---
+++ HRMS/Menu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web;
+using HRIMS;
+namespace HRMS
+{
+    
+  public class Menu
+    {
+        
+        public static DataTable MenuTable()
+        {
+            try
+            {
+                DataTable dt=null;
+                if (HttpContext.Current.User.Identity.IsAuthenticated)
+                {
+                    int id=Convert.ToInt32(HttpContext.Current.User.Identity.Name);
+                    cEmpLogin emp = cEmpLogin.Get_ID(id);
+                    int accessId = emp.objRoleAccess.iObjectID;
+                    string strCon = ConfigurationManager.ConnectionStrings["DevA4.DatabaseConnectionString"].ConnectionString;
+                    SqlConnection conn = new SqlConnection(strCon);
+                    SqlDataAdapter adpt = new SqlDataAdapter("uspMenu", conn);
+                    adpt.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    adpt.SelectCommand.Parameters.AddWithValue("@id",accessId);
+                    conn.Open();
+                    adpt.SelectCommand.ExecuteReader();
+                    conn.Close();
+                    dt = new DataTable();
+                    adpt.Fill(dt);
+                    
+                }
+                return dt;
+                
+            }
+            catch (Exception ex)
+            {
+                
+                throw ex;
+            }
+            
+        }
+    }
+}
---
+++ HRMS/menurole.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using HRIMS;
+namespace HRMS
+{
+
+   public class menurole
+    {
+       public static DataTable getMenu()
+       {
+           try
+           {
+               int id = Convert.ToInt32(HttpContext.Current.User.Identity.Name);
+               cEmpLogin emp = cEmpLogin.Get_ID(id);
+               int accessId = emp.objRoleAccess.iObjectID;
+               string cs = System.Configuration.ConfigurationManager.ConnectionStrings["DevA4.DatabaseConnectionString"].ConnectionString;
+
+               SqlConnection con = new SqlConnection(cs);
+               SqlDataAdapter ad = new SqlDataAdapter("uspfindMenu", con);
+               ad.SelectCommand.CommandType = CommandType.StoredProcedure;
+               ad.SelectCommand.Parameters.AddWithValue("@id", accessId);
+               con.Open();
+               ad.SelectCommand.ExecuteReader();
+               con.Close();
+               DataTable dt = null;
+               dt = new DataTable();
+               ad.Fill(dt);
+               return dt;
+           }
+           catch (Exception ex)
+           {
+               
+               throw ex;
+           }
+         
+       }
+      
+
+    }
+}
